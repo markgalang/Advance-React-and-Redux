a1e9db7173ff408cb2155427b452ac5ce978b449
Mark Galang <galangmarkg@gmail.com>
2020-08-05 16:04:33 +0800

    CommentList Component

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => {
-  return <div>Comment List</div>;
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map((comment) => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return <div>{this.renderComments()}</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { comments: state.comments };
 };
+export default connect(mapStateToProps)(CommentList);
