07e6a0ad6eafdce322e7fd2721d732e456036b9f
Mark Galang <galangmarkg@gmail.com>
2020-08-06 16:53:50 +0800

    API calls boiler plate

--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,10 +1,14 @@
-import { SAVE_COMMENT } from "actions/types";
+import { SAVE_COMMENT, FETCH_COMMENTS } from "actions/types";
 
 export default function (state = [], action) {
   switch (action.type) {
     case SAVE_COMMENT:
       return [...state, action.payload];
 
+    case FETCH_COMMENTS:
+      const commentNames = action.payload.data.map((comment) => comment.name);
+
+      return [...state, ...commentNames];
     default:
       return state;
   }
